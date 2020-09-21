e56ebbb437cc046c85e8159dffce3dc4e46d8f70
Juliette Rapala <jrapala@gmail.com>
2020-09-21 12:08:56 -0500

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by Juliette Rapala on 9/21/20.
+//  Copyright © 2020 Juliette Rapala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
