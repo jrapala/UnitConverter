4f350f24801abdc2f0f7e304d395b74de1a7bb07
Juliette Rapala <jrapala@gmail.com>
2020-09-22 10:31:00 -0500

    feat: add time conversion logic and TODOs

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,13 +8,55 @@
 
 import SwiftUI
 
+// TODO: Use enums
+// TODO: DRY code
+// TODO: Temperature conversion: users choose Celsius, Fahrenheit, or Kelvin.
+// TODO: Length conversion: users choose meters, kilometers, feet, yards, or miles.
+// TODO: Volume conversion: users choose milliliters, liters, cups, pints, or gallons.
+// TODO: Use Apple's unit types. More info @ https://www.hackingwithswift.com/example-code/system/how-to-convert-units-using-unit-and-measurement
+
 struct ContentView: View {
     @State private var valueToConvert = ""
     @State private var fromUnit = 0
     @State private var toUnit = 0
-    @State private var result = 0
 
     let timeUnits = ["seconds", "minutes", "hours", "days"]
+
+    func convertToSeconds(_ value: Double) -> Double {
+        switch timeUnits[fromUnit] {
+            case "seconds":
+                return value
+            case "minutes":
+                return value * 60
+            case "hours":
+                return value * 60 * 60
+            case "days":
+                return value * 60 * 60 * 24
+            default:
+                return value
+        }
+    }
+    
+    func convertSecondsToUnit(_ value: Double) -> Double {
+        switch timeUnits[toUnit] {
+            case "seconds":
+                return value
+            case "minutes":
+                return value / 60
+            case "hours":
+                return value / 60 / 60
+            case "days":
+                return value / 60 / 60 / 24
+            default:
+                return value
+        }
+    }
+
+    var result: Double {
+        let input = Double(valueToConvert) ?? 0
+        let inputInSeconds = convertToSeconds(input)
+        return convertSecondsToUnit(inputInSeconds)
+    }
 
     var body: some View {
         NavigationView {
@@ -37,11 +79,11 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section(header: Text("Value to convert")) {
+                Section(header: Text("Value to convert (in \(timeUnits[fromUnit]))")) {
                     TextField("Enter a value", text: $valueToConvert)
                 }
                 
-                Section(header: Text("Result")) {
+                Section(header: Text("Result (in \(timeUnits[toUnit]))")) {
                     Text("\(result)")
                 }
                 
