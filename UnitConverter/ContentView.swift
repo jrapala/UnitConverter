de02cbb22bac35cc90b445c5c8b4a26a0b3420d2
Juliette Rapala <jrapala@gmail.com>
2020-09-21 20:30:34 -0500

    feat: add time conversion UI

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var valueToConvert = ""
+    @State private var fromUnit = 0
+    @State private var toUnit = 0
+    @State private var result = 0
+
+    let timeUnits = ["seconds", "minutes", "hours", "days"]
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("Convert from")) {
+                    Picker("Convert From", selection: $fromUnit) {
+                        ForEach(0 ..< timeUnits.count) {
+                            Text("\(self.timeUnits[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Convert to")) {
+                    Picker("Convert To", selection: $toUnit) {
+                        ForEach(0 ..< timeUnits.count) {
+                            Text("\(self.timeUnits[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Value to convert")) {
+                    TextField("Enter a value", text: $valueToConvert)
+                }
+                
+                Section(header: Text("Result")) {
+                    Text("\(result)")
+                }
+                
+            }
+            .navigationBarTitle("Time Converter")
+        }
     }
 }
 
